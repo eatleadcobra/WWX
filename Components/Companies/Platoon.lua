dabe1e913e9ba5770bb157cf2107cfd834f97f8d
eatleadcobra <shippjohn44@gmail.com>
2025-02-04 10:54:45 -0600

    Spawning working

--- Components/Companies/Platoon.lua
+++ Components/Companies/Platoon.lua
@@ -1,10 +1,30 @@
-local pltTypes = {
+PlatoonTypes = {
     [1] = "Armor",
     [2] = "Mech",
     [3] = "Inf"
 }
-Platoon = {
-    id = 0,
-    pltType = "",
-    strength = 100
+Platoons = {
+    ["Armor"] = {
+        [1] = "Tank",
+        [2] = "Tank",
+        [3] = "Tank",
+        [4] = "Truck"
+    },
+    ["Mech"] = {
+        [1] = "APC",
+        [2] = "APC",
+        [3] = "APC",
+        [4] = "Truck"
+    },
+    ["Inf"] = {
+        [1] = "IFV",
+        [2] = "Inf",
+        [3] = "Inf",
+        [4] = "MG",
+        [5] = "Inf",
+        [6] = "Inf",
+        [7] = "RPG",
+        [8] = "Inf",
+        [9] = "Inf",
+    }
 }
