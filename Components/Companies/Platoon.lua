1c80c6bb509d144c148bc06f5edbc9475e62bbe8
eatleadcobra <shippjohn44@gmail.com>
2025-02-04 15:29:10 -0600

    working but deployment sucks going to change it

--- Components/Companies/Platoon.lua
+++ Components/Companies/Platoon.lua
@@ -7,24 +7,16 @@ Platoons = {
     ["Armor"] = {
         [1] = "Tank",
         [2] = "Tank",
-        [3] = "Tank",
         [4] = "Truck"
     },
     ["Mech"] = {
-        [1] = "APC",
-        [2] = "APC",
-        [3] = "APC",
-        [4] = "Truck"
+        [1] = "IFV",
+        [2] = "IFV",
+        [3] = "Truck"
     },
     ["Inf"] = {
-        [1] = "IFV",
-        [2] = "Inf",
-        [3] = "Inf",
-        [4] = "MG",
-        [5] = "Inf",
-        [6] = "Inf",
-        [7] = "RPG",
-        [8] = "Inf",
-        [9] = "Inf",
+        [1] = "APC",
+        [2] = "APC",
+        [3] = "Truck"
     }
 }
