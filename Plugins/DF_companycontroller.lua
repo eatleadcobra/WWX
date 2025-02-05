a7a7acf060d455baa12e4a8611a1a0b249e04c67
eatleadcobra <shippjohn44@gmail.com>
2025-02-05 14:18:38 -0600

    company persistence working

--- Plugins/DF_companycontroller.lua
+++ Plugins/DF_companycontroller.lua
@@ -3,17 +3,68 @@
 --undeploy mobile troops when they are moving
 --remove lost units from available
 --persist and have provisions to respawn companies on mission load
+local cpyctl = {}
+function cpyctl.fileExists(file)
+    local f = io.open(file, 'rb')
+    if f then f:close() end
+    return f ~= nil
+end
+
 Companies = {}
 CompanyIDs = {
     [1] = {},
     [2] = {}
 }
+local missionName = env.mission["date"]["Year"]
+local companyState = lfs.writedir() .. [[Logs/]] .. 'companies'..missionName..'.txt'
+function cpyctl.saveCompanies()
+    local cpyFile = companyState
+    local f = io.open(cpyFile, 'w')
+    local companiesData = {}
+    for k,v in pairs(Companies) do
+        local cpyData = {
+            id = v.id,
+            coalitionId = v.coalitionId,
+            status = v.status,
+            statusChangedTime = v.statusChangedTime,
+            point = v.point,
+            units = v.units,
+            waypoints = v.waypoints,
+            groupName = v.groupName,
+            deployedGroupNames = v.deployedGroupNames,
+            arrived = v.arrived
+        }
+        companiesData[v.id] = cpyData
+    end
+    f:write("return " .. Utils.saveToString(companiesData))
+    f:close()
+end
+function cpyctl.getCompanies()
+    if cpyctl.fileExists(companyState) then
+        local f = io.open(companyState, 'r')
+        local cpyData = dofile(companyState)
+        for k,v in pairs(cpyData) do
+            local newCpy = Company.newFromTable(v)
+            Companies[newCpy.id] = newCpy
+            table.insert(CompanyIDs[newCpy.coalitionId], newCpy.id)
+        end
+        f:close()
+    end
+end
+function cpyctl.spawnCompanies()
+    for k,v in pairs(Companies) do
+        v:spawn()
+    end
+end
 
-
-local startPoint = trigger.misc.getZone(DFS.spawnNames[2].depot..2).point
-local destination = trigger.misc.getZone("BP-1").point
-local testCpy = Company.new(2, {1,2,3})
-testCpy:setWaypoints({startPoint, destination})
-testCpy:spawn()
-timer.scheduleFunction(Company.despawn, testCpy, timer:getTime() + 30)
-timer.scheduleFunction(Company.spawn, testCpy, timer:getTime() + 40)
+-- local startPoint = trigger.misc.getZone("spawn").point
+-- local destination = trigger.misc.getZone("BP-1").point
+-- local testCpy = Company.new(2, {1,2,3})
+-- Companies[testCpy.id] = testCpy
+-- table.insert(CompanyIDs[testCpy.coalitionId], testCpy.id)
+-- testCpy:setWaypoints({startPoint, destination})
+-- testCpy:spawn()
+-- testCpy:savePosition()
+-- cpyctl.saveCompanies()
+cpyctl.getCompanies()
+cpyctl.spawnCompanies()
