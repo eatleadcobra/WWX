c610cb85adb44ded85f49a184dd5bdbb0c6beea5
eatleadcobra <shippjohn44@gmail.com>
2025-06-04 16:18:45 -0500

    remove old frontline recon creation

--- Plugins/DF_recon.lua
+++ Plugins/DF_recon.lua
@@ -1,29 +1,6 @@
 local dfrecon = {}
 local reconInterval = 1800
 local missionExpireTime = 3600
-function dfrecon.loop()
-    for c = 1, 2 do
-        local enemyCoalition = 2
-        if c == 2 then enemyCoalition = 1 end
-        local missionGroupSpawn = DFS.status[enemyCoalition].spawns.front[math.random(#DFS.status[enemyCoalition].spawns.front)]
-        if missionGroupSpawn then
-            local missionGroupName = missionGroupSpawn.groupName
-            if missionGroupName then
-                local missionGroup = Group.getByName(missionGroupName)
-                if missionGroup then
-                    local missionUnit = missionGroup:getUnit(1)
-                    if missionUnit then
-                        local missionPoint = missionUnit:getPoint()
-                        if missionPoint then
-                            Recon.createEnemyLocationMission(c, missionPoint, missionGroupName)
-                        end
-                    end
-                end
-            end
-        end
-    end
-    timer.scheduleFunction(dfrecon.loop, nil, timer:getTime() + reconInterval)
-end
 function dfrecon.depotLoop()
     for c = 1, 2 do
         local enemyCoalition = 2
