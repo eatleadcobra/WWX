84119eb3ab4d9a3ce769131c7bd1a3e66dfdcbd0
mobot-gh <moezilla@gmail.com>
2025-06-14 12:01:26 +0100

    [CSARBot] move RandomNames back to WWXLoader and update names via overrides

--- Overrides/overrideLoaderCyprus.lua
+++ Overrides/overrideLoaderCyprus.lua
@@ -3,4 +3,3 @@ if DEBUG then
    pathToWWX = "F:\\Games\\WWX\\"
 end
 assert(loadfile(pathToWWX.."Overrides\\Cyprus.lua"))()
-assert(loadfile(pathToWWX.."Overrides\\RandomNames_Cyprus.lua"))()
--- Overrides/overrideLoaderGermay.lua
+++ Overrides/overrideLoaderGermay.lua
@@ -3,4 +3,3 @@ if DEBUG then
    pathToWWX = "F:\\Games\\WWX\\"
 end
 assert(loadfile(pathToWWX.."Overrides\\GermanyCW.lua"))()
-assert(loadfile(pathToWWX.."Overrides\\RandomNames_Germany70.lua"))()
