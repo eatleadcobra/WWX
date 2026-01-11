4368154253c24cdcfb3f427abf01b2117c3881fe
TheSpudmonkey <cameron_ricketts@bigpond.com>
2026-01-11 12:24:28 +1100

    Add WWX Battle of Aleppo V1.0.3 (#56)
    * Add WWX Battle of Aleppo 1.0.3
    
    * Added overrides for Battle of Aleppo V1.0.3
    
    * Added dynamic spawn files for Battle of Aleppo V1.0.3

---
+++ Utils/DynamicSpawns/AleppoTemplate.lua
@@ -0,0 +1,122 @@
+Airframes = {
+    [1] = {
+        main = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+            ["Ka-50"] = true,
+            ["Ka-50_3"] = true,
+            ["C-130J-30"] = true,
+            ["MiG-19P"] = true,
+            ["MiG-21Bis"] = true,
+			["MiG-29 Fulcrum"] = true,
+            ["Mirage-F1CE"] = true,
+			["JF-17"] = true,
+            ["AJS37"] = true,
+            ["Su-25"] = true,
+            ["Su-25T"] = true,
+            ["Yak-52"] = true,
+            ["L-39C"] = true,
+            ["L-39ZA"] = true,
+			["C-101CC"] = true,
+        },
+        forward = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+            ["Ka-50"] = true,
+            ["Ka-50_3"] = true,
+            ["C-130J-30"] = true,
+            ["MiG-19P"] = true,
+            ["MiG-21Bis"] = true,
+			["MiG-29 Fulcrum"] = true, --with 27R and 60M only
+            ["Mirage-F1CE"] = true,
+            ["AJS37"] = true,
+            ["Su-25"] = true,
+            ["Su-25T"] = true,
+            ["Yak-52"] = true,
+            ["L-39C"] = true,
+            ["L-39ZA"] = true,
+			["C-101CC"] = true,
+        },
+        farp = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+            ["Ka-50"] = true,
+            ["Ka-50_3"] = true,
+        }
+    },
+    [2] = {
+        main = {
+            ["SA342L"] = true,
+			["SA342M"] = true,
+			["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["AH-64D_BLK_II"] = true,
+            ["CH-47Fbl1"] = true,
+			["C-130J-30"] = true,
+			["M-2000C"] = true,
+			["Mirage-F1EE"] = true,
+			["Mirage-F1BE"] = true,
+			["F-5E-3"] = true,
+			["F-15ESE"] = true,
+            ["F-16C_50"] = true,
+			["FA-18C_hornet"] = true,
+            ["A-10A"] = true,
+            ["A-10C"] = true,
+            ["A-10C_2"] = true,
+            ["AV8BNA"] = true,
+            ["F-4E-45MC"] = true,
+			["F-14A-135-GR"] = true, --F-111 standin
+			["MB-339A"] = true,
+        },
+        forward = {
+            ["SA342L"] = true,
+			["SA342M"] = true,
+			["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["AH-64D_BLK_II"] = true,
+            ["CH-47Fbl1"] = true,
+			["C-130J-30"] = true,
+			["M-2000C"] = true, --with Magic II only
+			["Mirage-F1EE"] = true,
+			["Mirage-F1BE"] = true,
+			["F-5E-3"] = true,
+            ["A-10A"] = true,
+            ["A-10C"] = true,
+            ["A-10C_2"] = true,
+            ["AV8BNA"] = true,
+            ["F-4E-45MC"] = true,
+			["MB-339A"] = true,
+        },
+        farp = {
+            ["SA342L"] = true,
+			["SA342M"] = true,
+			["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["AH-64D_BLK_II"] = true,
+            ["CH-47Fbl1"] = true,
+        }
+    }
+}
+ForwardAirbases = {
+    [1] = {
+        ["Aleppo"] = true,
+		["Kuweires"] = true,
+		["Abu al-Duhur"] = true,
+    },
+    [2] = {
+        ["Aleppo"] = true,
+		["Hatay"] = true,
+		["Bassel Al-Assad"] = true,
+    }
+}
+IgnoreAirbases = {
+    ["CV-59"] = true,
+    ["LHA-1"] = true,
+	["LPD-71"] = true,
+	["Kuweires"] = true,
+	["Taftanaz"] = true,
+	["Minakh"] = true,
+}
