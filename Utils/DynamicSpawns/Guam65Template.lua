06b3ef92150455597f9826a7c8977317fead3262
TheSpudmonkey <cameron_ricketts@bigpond.com>
2026-01-26 10:07:20 +1100

    Added Battle of Guam65 (#70)
    * Add Battle of Guam65
    
    * Add overrides for Battle of Guam65
    
    * Add spawn templates for Battle of Guam 65

---
+++ Utils/DynamicSpawns/Guam65Template.lua
@@ -0,0 +1,63 @@
+Airframes = {
+    [1] = {
+        main = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+			['MiG-15bis'] = true,
+            ["MiG-19P"] = true,
+            ["MiG-21Bis"] = true,
+            ["Yak-52"] = true,
+            ["L-39C"] = true,
+            ["L-39ZA"] = true,
+			--["C-130J-30"] = true,
+        },
+        forward = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+			["Yak-52"] = true,
+        },
+        farp = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+        }
+    },
+    [2] = {
+        main = {
+			["UH-1H"] = true,
+			["CH-47Fbl1"] = true,
+			["OH58D"] = true,
+			["F-86F Sabre"] = true,
+			["F-4E-45MC"] = true,
+			["F-5E-3"] = true,
+			["Mirage-F1CE"] = true,
+			["MB-339A"] = true,
+            ["C-101CC"] = true,
+            ["C-130J-30"] = true,
+        },
+        forward = {
+            ["UH-1H"] = true,
+			["CH-47Fbl1"] = true,
+			["OH58D"] = true,
+        },
+        farp = {
+            ["UH-1H"] = true,
+			["CH-47Fbl1"] = true,
+			["OH58D"] = true,
+        }
+    }
+}
+ForwardAirbases = {
+    [1] = {
+        --["Agana"] = true,
+    },
+    [2] = {
+        --["Agana"] = true,
+    }
+}
+IgnoreAirbases = {
+    --["CV59"] = true,
+    --["LHA-1"] = true,
+}
