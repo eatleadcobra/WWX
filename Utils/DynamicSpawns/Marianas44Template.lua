6e05c29832fe7dc4b7222728f3fe3037dc871e28
TheSpudmonkey <cameron_ricketts@bigpond.com>
2026-02-07 23:53:10 +1100

    Add Battle of Marianas 44 (#80)
    * Add Battle of Marianas44
    
    * Add overrides for Battle of Marianas44
    
    * Add dynamic spawn files for Battle of Marianas44

---
+++ Utils/DynamicSpawns/Marianas44Template.lua
@@ -0,0 +1,57 @@
+Airframes = {
+    [1] = {
+        main = {
+            ["FW-190A8"] = true,
+            ["FW-190D9"] = true,
+            ["Bf-109K-4"] = true,
+			["I-16"] = true,
+            ["C-101CC"] = true,
+        },
+        forward = {
+        },
+        farp = {
+        }
+    },
+    [2] = {
+        main = {
+			["P-47D-30bl1"] = true,
+			["P-47D-30"] = true,
+			["P-47D-40"] = true,
+			["P-51D"] = true,
+			["MosquitoFBMkVI"] = true,
+			["SpitfireLFMkIX"] = true,
+			["SpitfireLFMkIXCW"] = true,
+			["SpitfireLFMkIXCW"] = true,
+			["F4U-1D"] = true,
+			["F4U-1D_CW"] = true,
+        },
+        forward = {
+        },
+        farp = { --Carrier
+			["P-47D-30bl1"] = true,
+			["P-47D-30"] = true,
+			["P-47D-40"] = true,
+			["P-51D"] = true,
+			["MosquitoFBMkVI"] = true,
+			["SpitfireLFMkIX"] = true,
+			["SpitfireLFMkIXCW"] = true,
+			["SpitfireLFMkIXCW"] = true,
+			["F4U-1D"] = true,
+			["F4U-1D_CW"] = true,
+        }
+    }
+}
+ForwardAirbases = {
+    [1] = {
+        --["Agana"] = true,
+    },
+    [2] = {
+        --["Blue-Carrier-Landing-1-1"] = true,
+		["Ushi"] = true,
+    }
+}
+IgnoreAirbases = {
+    ["Blue-Carrier-Landing-1-1"] = true,
+	--["Blue-Carrier-HotStart-1-1"] = true,
+	--["Blue-Carrier-ColdStart-1-1"] = true,
+}
