7215c0e3d1dcf7449bc3d42fbfb939fd94128227
eatleadcobra <shippjohn44@gmail.com>
2026-02-23 19:20:45 -0600

    Fixing ww2 template

--- Utils/DynamicSpawns/FreeWV2Template.lua
+++ Utils/DynamicSpawns/FreeWV2Template.lua
@@ -1,20 +1,20 @@
 Airframes = {
     [1] = {
         main = {
-            --garbage: soulful
             ["C-130J-30"] = true,
             --warbirds
             ["P-47D-30bl1"] = true,
             ["P-47D-30"] = true,
             ["P-47D-40"] = true,
-            ["FW-190A8"] = true,
-            ["FW-190D9"] = true,
-            ["Bf-109K-4"] = true,
+            ["SpitfireLFMkIX"] = true,
+            ["SpitfireLFMkIXCW"] = true,
+            ["P-51D"] = true,
+            ["I-16"] = true,
             --FAC
-            ["Yak-52"] = true,
+            ["TF-51D"] = true,
             --Helos
             ["Mi-8MT"] = true,
-            ["OH58D"] = true,
+            ["SA342L"] = true,
         },
         carrier = {
         },
@@ -22,26 +22,22 @@ Airframes = {
         },
         farp = {
             ["Mi-8MT"] = true,
-            ["OH58D"] = true,
-            ["CH-47Fbl1"] = true,
+            ["SA342L"] = true,
         }
     },
     [2] = {
         main = {
-            --garbage: soulful
-            --["C-130J-30"] = true,
             --warbirds
+            ["FW-190A8"] = true,
+            ["FW-190D9"] = true,
+            ["Bf-109K-4"] = true,
             ["MosquitoFBMkVI"] = true,
-            ["SpitfireLFMkIX"] = true,
-            ["SpitfireLFMkIXCW"] = true,
-            ["I-16"] = true,
-            ["P-51D"] = true,
             ["F4U-1D"] = true,
             --FAC
-            ["TF-51D"] = true,
+            ["Yak-52"] = true,
             --Helos
             ["UH-1H"] = true,
-            ["SA342L"] = true,
+            ["OH58D"] = true,
             ["CH-47Fbl1"] = true,
         },
         carrier = {
@@ -50,7 +46,7 @@ Airframes = {
         },
         farp = {
             ["UH-1H"] = true,
-            ["SA342L"] = true,
+            ["OH58D"] = true,
             ["CH-47Fbl1"] = true,
         }
     }
