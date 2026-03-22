ac549d28dcf912b12bac52a7b841198c0e89e570
George Brooker <87795533+GeorgeBrooker@users.noreply.github.com>
2026-03-23 03:01:45 +1300

    Update Marianas, add battle of the baltic 1.0 (#96)
    * Add Battle of Marianas v1.0.2 (#18)
    
    * Add Battle of Marianas 44 v1.0.2
    
    * Change override for Battle of Marianas44 v1.0.2
    
    * Change template for Battle of Marianas44 v1.0.2
    
    * Update Battle of Marianas 44 v1.0.2 override (#19)
    
    * Update Battle of Marianas 44 v1.0.2 override
    
    * Change override for Battle of Fulda87 v1.1.5
    
    * Add Battle of Fulda87 v1.1.5
    
    * Add Battle of the Baltic45 v1.0.0 (#20)
    
    * Add Battle of the Baltic45 v1.0.0
    
    * Add overrides for Battle of the Baltic45 v1.0.0
    
    * Add templates for Battle of the Baltic45 v1.0.0
    
    * Fix for Marianas44
    
    * Update Battle of Marianas 44 v1.0.2 override (#19)
    
    * Update Battle of Marianas 44 v1.0.2 override
    
    * Change override for Battle of Fulda87 v1.1.5
    
    * Add Battle of Fulda87 v1.1.5
    
    * Add Battle of the Baltic45 v1.0.0 (#20)
    
    * Add Battle of the Baltic45 v1.0.0
    
    * Add overrides for Battle of the Baltic45 v1.0.0
    
    * Add templates for Battle of the Baltic45 v1.0.0
    
    * Fix for Marianas44
    
    * Fix template for Battle of the Baltic45 v1.0.1
    
    * Add Battle of the Baltic45 v1.0.1
    
    * Add Battle of the Baltic45 v1.0.2
    
    ---------
    
    Co-authored-by: TheSpudmonkey <cameron_ricketts@bigpond.com>

---
+++ Utils/DynamicSpawns/Baltic45Template.lua
@@ -0,0 +1,59 @@
+Airframes = {
+    [1] = {
+        main = {
+            ["P-47D-30bl1"] = true,
+			["P-47D-30"] = true,
+			["P-47D-40"] = true,
+			["P-51D"] = true,
+			["MosquitoFBMkVI"] = true,
+			["SpitfireLFMkIX"] = true,
+			["SpitfireLFMkIXCW"] = true,
+			["I-16"] = true,
+			["La-7"] = true,
+        },
+        forward = {
+        },
+        carrier = {
+        },
+        farp = {
+        }
+    },
+    [2] = {
+        main = {
+			["FW-190A8"] = true,
+            ["FW-190D9"] = true,
+            ["Bf-109K-4"] = true,
+			["F4U-1D"] = true,
+			["F4U-1D_CW"] = true,
+            ["C-101CC"] = true,
+        },
+        forward = {
+        },
+        carrier = {
+        },
+        farp = {
+        }
+    }
+}
+ForwardAirbases = {
+    [1] = {
+    },
+    [2] = {
+    }
+}
+FARPAirfields = {
+    [1] = {
+    },
+    [2] = {
+    }
+}
+--carrier names are the UNIT name, NOT the group name
+Carrier = {
+    [1] = {
+    },
+    [2] = {
+    }
+}
+
+IgnoreAirbases = {
+}
