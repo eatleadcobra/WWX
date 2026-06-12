81005b9fe986c8b0306b55720891687ae311862a
eatleadcobra <26315464+eatleadcobra@users.noreply.github.com>
2026-06-12 18:38:41 -0500

    Add F-100D to Lebanon

---
+++ Utils/DynamicSpawns/HatayTemplate.lua
@@ -0,0 +1,40 @@
+Airframes = {
+    [1] = {
+        main = {
+
+        },
+        carrier = {
+        },
+        forward = {
+        },
+        farp = {
+
+        }
+    },
+    [2] = {
+        main = {
+
+        },
+        carrier = {
+        },
+        forward = {
+        },
+        farp = {
+        }
+    }
+}
+ForwardAirbases = {
+    [1] = {
+    },
+    [2] = {},
+}
+FARPAirfields = {
+    [1] = {},
+    [2] = {},
+}
+Carriers = {
+    [1] = {},
+    [2] = {},
+}
+IgnoreAirbases = {
+}
