5abfa0d923675bb023030eb086058d9b9d298e28
eatleadcobra <26315464+eatleadcobra@users.noreply.github.com>
2026-06-14 15:26:13 -0500

    Cooking up some fresh slop

--- Utils/DynamicSpawns/HatayTemplate.lua
+++ Utils/DynamicSpawns/HatayTemplate.lua
@@ -1,40 +1,77 @@
 Airframes = {
     [1] = {
         main = {
-
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+            ["C-130J-30"] = true,
+            ['MiG-15bis'] = true,
+            ["MiG-19P"] = true,
+            ["MiG-21Bis"] = true,
+            ["Mirage-F1CE"] = true,
+            ["Yak-52"] = true,
+            ["L-39C"] = true,
+            ["L-39ZA"] = true,
         },
-        carrier = {
-        },
+        carrier = {},
         forward = {
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
+            ['MiG-15bis'] = true,
+            ["Yak-52"] = true,
+            ["L-39C"] = true,
+            ["L-39ZA"] = true,
         },
         farp = {
-
+            ["Mi-24P"] = true,
+            ["Mi-8MT"] = true,
+            ["SA342L"] = true,
         }
     },
     [2] = {
         main = {
-
+            ["TF-51D"] = true,
+            ["MosquitoFBMkVI"] = true,
+            ["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["CH-47Fbl1"] = true,
+            ["AJS37"] = true,
+            ["C-130J-30"] = true,
+            ["C-101CC"] = true,
+            ["MB-339A"] = true,
+            ["F-86F Sabre"] = true,
+            ["F-100D"] = true,
+            ["F-5E-3"] = true,
         },
-        carrier = {
-        },
+        carrier = {},
         forward = {
+            ["TF-51D"] = true,
+            ["MosquitoFBMkVI"] = true,
+            ["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["CH-47Fbl1"] = true,
+            ["F-86F Sabre"] = true,
         },
         farp = {
+            ["UH-1H"] = true,
+            ["OH58D"] = true,
+            ["CH-47Fbl1"] = true,
         }
     }
 }
 ForwardAirbases = {
-    [1] = {
-    },
+    [1] = {},
     [2] = {},
 }
 FARPAirfields = {
-    [1] = {},
+    [1] = {
+        ["Minakh"] = true
+    },
     [2] = {},
 }
 Carriers = {
     [1] = {},
     [2] = {},
 }
-IgnoreAirbases = {
-}
+IgnoreAirbases = {}
